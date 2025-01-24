ccb16dabb260523462aa4e3c926a8c7e76ce9faa
Simran Sandhu <54254811+simran7s@users.noreply.github.com>
2025-01-24 17:07:45 +0000

    display game board

--- zustand-tic-tac-toe/src/components/board.tsx
+++ zustand-tic-tac-toe/src/components/board.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react/jsx-runtime';
 import { useGameStore } from '../store/use-game-store';
 import { calculateStatus, calculateTurns, calculateWinner } from '../utils/utils';
 import Square from './square';
@@ -25,23 +26,26 @@ export default function Board() {
       }
 
     return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(3, 1fr)',
-          gridTemplateRows: 'repeat(3, 1fr)',
-          width: 'calc(3 * 2.5rem)',
-          height: 'calc(3 * 2.5rem)',
-          border: '1px solid #999',
-        }}
-      >
-      {squares.map((square, squareIndex) => (
-        <Square 
-            key={squareIndex} 
-            value={square} 
-            onSquareClick={() => handleClick(squareIndex)}
-        />
-      ))}
-      </div>
+    <Fragment>
+        <div style={{ marginBottom: '0.5rem' }}>{status}</div>
+        <div
+            style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gridTemplateRows: 'repeat(3, 1fr)',
+            width: 'calc(3 * 2.5rem)',
+            height: 'calc(3 * 2.5rem)',
+            border: '1px solid #999',
+            }}
+        >
+            {squares.map((square, squareIndex) => (
+                <Square 
+                    key={squareIndex} 
+                    value={square} 
+                    onSquareClick={() => handleClick(squareIndex)}
+                />
+            ))}
+        </div>
+    </Fragment>
     )
   }
